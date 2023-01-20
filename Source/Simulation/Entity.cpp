a778d2de1c513803ccfbdaac098854ea64949d30
deniskup <denis.kuperberg@gmail.com>
2023-01-20 01:11:12 +0100

    threshold energy

--- Source/Simulation/Entity.cpp
+++ Source/Simulation/Entity.cpp
@@ -7,7 +7,7 @@ Entity::Entity(var params) : BaseItem(getTypeString() + " 1")
   creationRate = addFloatParameter("Creation rate", "Creation rate of the entity", .1f, .0f, 1.f); // absolute
   destructionRate = addFloatParameter("Destruction rate", "Destruction rate of the entity", .1f, .0f, 1.f);// proportional to concentration
   concent = addFloatParameter("Concentration", "Concentration of the entity", .5f, .0f, 2.f);
-  freeEnergy = addFloatParameter("Free energy", "Free energy of the entity", 0.f, -10.f, 10.f);
+  freeEnergy = addFloatParameter("Free energy", "Free energy of the entity", 0.f, -20.f, 10.f);
   setHasCustomColor(true);
 }
 
