06dc2c5eefd16fe11456c5c95a831eb297f6719a
deniskup <denis.kuperberg@gmail.com>
2024-09-28 12:04:27 +0200

    name change in jucer

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -39,7 +39,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "EmergenceNS";
+    const char* const  projectName    = "EmergeNS";
     const char* const  companyName    = "";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
