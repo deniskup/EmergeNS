1c785dca57cb154068a7fdd5683253e8c8fa6d4c
thkosc <kosc.thomas@gmail.com>
2025-06-20 15:58:15 +0200

    Progress in NEPUI implementation

--- Source/Simulation/ui/NEPUI.h
+++ Source/Simulation/ui/NEPUI.h
@@ -7,39 +7,52 @@ using namespace std;
 
 
 class NEPUI : public ShapeShifterContentComponent,
-              public NEP::NEPListener
+              public NEP::AsyncNEPListener,
+              public ContainerAsyncListener,
+              public Timer
 {
 public:
     NEPUI();
     ~NEPUI();
 
-    NEP * pp;
+    NEP * nep;
     
     std::unique_ptr<GenericControllableContainerEditor> editorUI;
   
-    unique_ptr<TriggerUI> startUI;
-    unique_ptr<TriggerUI> drawUI;
-    unique_ptr<TriggerUI> startDrawUI;
   
-    unique_ptr<IntStepperUI> nRunsUI;
+    unique_ptr<TriggerUI> startDescentUI;
+    unique_ptr<TriggerUI> start_heteroclinic_studyUI;
+    unique_ptr<EnumParameterUI> sst_stableUI;
+    unique_ptr<EnumParameterUI> sst_saddleUI;
+    unique_ptr<IntStepperUI> NiterationsUI;
+    unique_ptr<IntStepperUI> nPointsUI;
+    unique_ptr<FloatParameterLabelUI> cutoffFreqUI;
+    unique_ptr<FloatParameterLabelUI> action_thresholdUI;
+    unique_ptr<FloatParameterLabelUI> timescale_factorUI;
 
-    juce::Viewport vp;
+  
+    juce::Viewport vp; // scrollable window
 
+    // Action array
+    Array<int> iterations;
+    Array<double> actions;
+  
+    void paintOneMonitoredQuantity(juce::Graphics &, Rectangle<int>, String, Array<double>);
 
-    // std::unique_ptr<IntStepperUI> numLevelsUI;
-    // std::unique_ptr<IntStepperUI> entitiesPerLevelUI;
-    // std::unique_ptr<IntStepperUI> maxReactionsPerEntityUI;
-    // std::unique_ptr<IntStepperUI> avgNumShowUI;
-    
-    //local floatparameter
-    //std::unique_ptr<FloatParameter> maxC;
-    //std::unique_ptr<FloatParameterLabelUI> maxCUI;
-
-    //void paint(juce::Graphics &) override;
+    void paint(juce::Graphics &) override;
+  
     void resized() override;
+  
+    void timerCallback() override;
+  
+    bool keyPressed(const KeyPress &e) override;
+
+    void newMessage(const NEP::NEPEvent &e) override;
+
+    void newMessage(const ContainerAsyncEvent &e) override;
 
     static NEPUI *create(const String &name) { return new NEPUI(); }
 
-    void updateNEPUI(NEP *) override;
+    //void updateNEPUI(NEP *);
 
 };
