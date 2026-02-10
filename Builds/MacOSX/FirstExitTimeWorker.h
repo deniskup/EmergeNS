c670bbfc27caefa0d0025c8d620c40f765001e69
thkosc <kosc.thomas@gmail.com>
2026-02-10 16:34:54 +0100

    Delegate heavy calculations of FirstExitStudy to a worker thread, avoiding clashes between Simulation listeners (compilation OK, not tested)

---
+++ Builds/MacOSX/FirstExitTimeWorker.h
@@ -0,0 +1,60 @@
+#include "JuceHeader.h"
+#include "Simulation/Simulation.h"
+#include "Simulation/KineticLaw.h"
+
+class FirstExitTime : public Simulation::AsyncSimListener
+                      //public ContainerAsyncListener
+{
+public:
+  juce_DeclareSingleton(FirstExitTime, true);
+  FirstExitTime();
+  ~FirstExitTime();
+  
+  void reset();
+  
+  void setSimulationConfig(std::map<String, String>);
+  
+  void startStudy();
+  
+  //void setConcToSteadyState(int);
+  
+private:
+  
+  int identifyAttractionBasin(ConcentrationGrid &, float);
+  
+  float distanceFromSteadyState(State sst);
+  
+  SimEntity * getSimEntityForID(const size_t);
+  
+  void printResultsToFile();
+  
+  void newMessage(const Simulation::SimulationEvent &e) override;
+
+  //void newMessage(const ContainerAsyncEvent &e) override;
+  
+  Simulation * simul;
+  KineticLaw * kinetics;
+  
+  OwnedArray<SimEntity> entities;
+  OwnedArray<SimReaction> reactions;
+
+  // to store escape times during this study
+  Array<float> escapeTimes;
+  
+  String networkfile = "./nextwork.txt";
+  String outputfilename = "./firstExitStudy.txt";
+  float precision = 1e-5; // precision up to which the steady state is determined
+  float exitTimePrecision = 10; // every 'exitTimePrecision', check where the system is
+  int superRun = 0;
+  int nruns = 1;
+  int startSteadyState = 0;
+  bool fixedSeed = false;
+  int seed = 1234;
+  int patchid = 0; // hardcoded patch in which this study takes place.
+  // In principle not designed to perform in heterogeneous space, it will complain about it otherwise
+  float dt_study = 0.1; // time step used to identify in which attraction basin the system is
+  bool printDynamics2File = false;
+  
+
+  
+};
