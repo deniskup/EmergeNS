3bb2502da526e6f4f9e02d6fa0b151aac8690d73
thkosc <kosc.thomas@gmail.com>
2026-03-31 15:01:22 +0200

    include windows.h

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,6 +1,19 @@
 #pragma once
 
 #include <JuceHeader.h>
+
+#if JUCE_WINDOWS
+ #ifndef NOMINMAX
+  #define NOMINMAX
+ #endif
+
+ #ifndef WIN32_LEAN_AND_MEAN
+  #define WIN32_LEAN_AND_MEAN
+ #endif
+
+#include <windows.h>
+#endif
+
 //using namespace juce;
 juce::ApplicationProperties &getAppProperties();
 juce::ApplicationCommandManager &getCommandManager();
